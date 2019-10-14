378554decb75ccd1fdaa09a794f6c46539015b08
Marina Ziembar <marinaziembar@gmail.com>
2019-10-14 02:14:32 -0300

    footer en contacto.php
    agregue el footer a la hoja de contacto para prueba

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="row">
+        <section class="col-md-7 col-xs-12  row listas">
+            <article class="col-md-2 col-xs-12">
+                <a href="#"><h5>Tienda</h5></a>
+                <ul>
+                    <li><a href="#">Monopatines</a></li>
+                    <li><a href="#">Accesorios</a></li>
+                </ul>
+            </article>
+            <article class="col-md-2 col-xs-12">
+                <a href="faq.html"><h5>Faq</h5></a> 
+            </article>
+
+            <article class="col-md-2 col-xs-12">
+                <a href="contacto.php"><h5>Contacto</h5></a>
+            </article>
+        </section>
+        <section class="col-md-5  col-xs-12 row logo">
+            <article class="col-12">
+                <a href="index.html"><h5>ROLLIN'</h5></a>
+                <a href="https://goo.gl/maps/x6Stu22dYpuu1bjT8" target="_blank"><p>Av. Lorem Ipsum Dolor 2563 - Winterfeld - Narnia</p></a>
+            </article>
+        </section>
+
+    </footer>
