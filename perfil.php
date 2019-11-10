cbccef2a6b1933bf7f188ba0b5926f0b038ebf8e
Marina Ziembar <marinaziembar@gmail.com>
2019-11-10 01:00:22 -0300

    modificaciones de perfil.php
    y de css

--- perfil.php
+++ perfil.php
@@ -12,7 +12,31 @@
 <?php
 require_once("nav.php");
 ?>
+<div class="perfil1">
+<h2>TU PERFIL</h2>
+<section class="perfil">
+<img src="IMAGENES/contactoDefault.png" alt="contactoFotoDefault">
+<div class="BOTONROJO">
+             <label for="foto">CAMBIAR FOTO</label>
+             <input id="foto" type="file" name="foto" value="">
+           </div>
+</section> 
 
+<!-- aca va php para cambiar datos de usuario -->
+<section class="perfil"> 
+<h3>NOMBRE APELLIDO</h3>
+<p>Email: nombre.apellido@gmail.com</p>
+<p>Fecha de nacimiento: </p>
+<p>Dirección: </p>
+<p>Teléfono: </p>
+<div class="BOTONROJO">
+<a href="#">EDITAR DATOS</a>
+</div>
+<div class="BOTONROJO">
+             <a href="#">VER COMPRAS</a>
+           </div>
+</section>
+</div>
 
 <?php
 require_once("footer.php");
