c47dac852ac762a476a1db0a02efadf6d0e77040
Marina Ziembar <marinaziembar@gmail.com>
2019-11-14 02:28:34 -0300

    Actualizo css y perfil.php
    cambios menores en html)

--- perfil.php
+++ perfil.php
@@ -12,30 +12,34 @@
 <?php
 require_once("nav.php");
 ?>
-<div class="perfil1">
+<div class="perfil">
 <h2>TU PERFIL</h2>
-<section class="perfil">
+<section class="perfil1">
 <img src="IMAGENES/contactoDefault.png" alt="contactoFotoDefault">
-<div class="BOTONROJO">
+<div class="BOTONROJO" id="botonPerfil">
              <label for="foto">CAMBIAR FOTO</label>
              <input id="foto" type="file" name="foto" value="">
            </div>
 </section> 
 
-<!-- aca va php para cambiar datos de usuario -->
-<section class="perfil"> 
+<!-- aca va php para cambiar, eliminar datos de usuario -->
+<section class="perfil1"> 
 <h3>NOMBRE APELLIDO</h3>
 <p>Email: nombre.apellido@gmail.com</p>
 <p>Fecha de nacimiento: </p>
 <p>Dirección: </p>
 <p>Teléfono: </p>
-<div class="BOTONROJO">
+<div class="BOTONROJO" id="botonPerfil">
 <a href="#">EDITAR DATOS</a>
 </div>
-<div class="BOTONROJO">
+<div class="BOTONROJO" id="botonPerfil">
              <a href="#">VER COMPRAS</a>
            </div>
-</section>
+<div class="BOTONROJO" id="botonPerfil">
+             <a href="#">ELIMINAR CUENTA</a>
+           </div>           
+</section>           
+
 </div>
 
 <?php
@@ -44,4 +48,6 @@ require_once("footer.php");
 
 </body>
 </html>
+
+
 
